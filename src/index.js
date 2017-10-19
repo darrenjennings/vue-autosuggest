108b1ce1a2ddcef625f2d4b30d6ceafe3988291f
darrenjennings <dmjenn02@gmail.com>
2017-10-18 21:30:17 -0700

    feat($compile): Added autoloading of umd module.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import Autosuggest from "./Autosuggest.vue";
 export default {
   install(Vue) {
     Vue.component("vue-autosuggest", Autosuggest);
+
+    // Automatically register component if Vue is available globally
+    if (typeof window !== "undefined" && window.Vue) {
+      window.Vue.use(Autosuggest);
+    }
   }
 };
