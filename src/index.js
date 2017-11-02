a2dcd923ef5b237bb1fb180d0065dcba3dd1bc85
Darren Jennings <dmjenn02@gmail.com>
2017-11-02 16:28:34 -0700

    Fixed install script.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import Autosuggest from "./Autosuggest.vue";
-import DefaultSection from './parts/DefaultSection.vue';
 
 const VueAutosuggest = {
   install(Vue) {
-    Vue.component('DefaultSection', DefaultSection);    
     Vue.component("vue-autosuggest", Autosuggest);
   }
 };
@@ -11,8 +9,5 @@ const VueAutosuggest = {
 export default VueAutosuggest;
 
 if (typeof window !== "undefined" && window.Vue) {
-  window.Vue.use(install);
-  if (install.installed) {
-    install.installed = false;
-  }
+  window.Vue.use(VueAutosuggest);
 }
