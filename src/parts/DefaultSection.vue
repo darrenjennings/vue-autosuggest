ae6d4b0287ffebf4e26fdf21b8ca4811cb008429
Darren Jennings <dmjenn02@gmail.com>
2017-11-18 21:13:45 -0800

    Tab indent spacing for prettier set back to 4.

--- src/parts/DefaultSection.vue
+++ src/parts/DefaultSection.vue
@@ -17,57 +17,57 @@
 
 <script>
 export default {
-  name: "default-section",
-  props: {
-    section: { type: Object, required: true },
-    currentIndex: { type: Number, required: false, default: Infinity },
-    updateCurrentIndex: { type: Function, required: true },
-    searchInput: { type: String, required: false, default: "" }
-  },
-  computed: {
-    list: function() {
-      var l = this.section.limit;
-      if (this.section.data.length < l) {
-        l = this.section.data.length;
-      }
-      return this.section.data.slice(0, l);
+    name: "default-section",
+    props: {
+        section: { type: Object, required: true },
+        currentIndex: { type: Number, required: false, default: Infinity },
+        updateCurrentIndex: { type: Function, required: true },
+        searchInput: { type: String, required: false, default: "" }
     },
-    className: function() {
-      return `autosuggest__results_title autosuggest__results_title_${this
-        .section.name}`;
-    }
-  },
-  methods: {
-    styleItem(text) {
-      if (
-        this.searchInput &&
-        typeof text === "string" &&
-        this.searchInput !== text
-      ) {
-        const value = this.searchInput.trim();
-        const r = new RegExp(`${value}`, "ig");
-        return text.replace(r, `<b>${value}</b>`);
-      }
+    computed: {
+        list: function() {
+            var l = this.section.limit;
+            if (this.section.data.length < l) {
+                l = this.section.data.length;
+            }
+            return this.section.data.slice(0, l);
+        },
+        className: function() {
+            return `autosuggest__results_title autosuggest__results_title_${this
+                .section.name}`;
+        }
+    },
+    methods: {
+        styleItem(text) {
+            if (
+                this.searchInput &&
+                typeof text === "string" &&
+                this.searchInput !== text
+            ) {
+                const value = this.searchInput.trim();
+                const r = new RegExp(`${value}`, "ig");
+                return text.replace(r, `<b>${value}</b>`);
+            }
 
-      return text;
-    },
-    getItemIndex(i) {
-      return this.section.start_index + i;
-    },
-    getItemByIndex(i) {
-      return this.section.data[i];
-    },
-    getLabelByIndex(i) {
-      return this.section.data[i];
-    },
-    onMouseEnter(event) {
-      this.updateCurrentIndex(
-        event.currentTarget.getAttribute("data-suggestion-index")
-      );
-    },
-    onMouseLeave() {
-      this.updateCurrentIndex(null);
+            return text;
+        },
+        getItemIndex(i) {
+            return this.section.start_index + i;
+        },
+        getItemByIndex(i) {
+            return this.section.data[i];
+        },
+        getLabelByIndex(i) {
+            return this.section.data[i];
+        },
+        onMouseEnter(event) {
+            this.updateCurrentIndex(
+                event.currentTarget.getAttribute("data-suggestion-index")
+            );
+        },
+        onMouseLeave() {
+            this.updateCurrentIndex(null);
+        }
     }
-  }
 };
 </script>
--- src/stories/index.js
+++ src/stories/index.js
@@ -5,92 +5,74 @@ import { storiesOf } from "@storybook/vue";
 import Autosuggest from "../Autosuggest.vue";
 
 const sharedData = {
-  options: [
-    "clifford kits",
-    "friendly chemistry",
-    "phonics",
-    "life of fred",
-    "life of fred math",
-    "magic school bus",
-    "math mammoth light blue",
-    "handwriting",
-    "math",
-    "minecraft",
-    "free worksheets",
-    "4th grade",
-    "snap circuits",
-    "bath toys",
-    "channies",
-    "fred",
-    "lego",
-    "math life of fred",
-    "multiplication",
-    "thinking tree"
-  ],
-  methods: {
-    onInputChange(text) {
-      if (text === null) {
-        return;
-      }
-      const filtered = [];
-      const suggestionsData = this.options[0].data.filter(item => {
-        return item.toLowerCase().indexOf(text.toLowerCase()) > -1;
-      });
-
-      suggestionsData.length > 0 &&
-        filtered.push({
-          label: "Suggestions",
-          data: suggestionsData
-        });
-
-      this.filteredOptions = filtered;
+    options: [
+        "clifford kits",
+        "friendly chemistry",
+        "phonics",
+        "life of fred",
+        "life of fred math",
+        "magic school bus",
+        "math mammoth light blue",
+        "handwriting",
+        "math",
+        "minecraft",
+        "free worksheets",
+        "4th grade",
+        "snap circuits",
+        "bath toys",
+        "channies",
+        "fred",
+        "lego",
+        "math life of fred",
+        "multiplication",
+        "thinking tree"
+    ],
+    methods: {
+        onInputChange(text) {
+            if (text === null) {
+                return;
+            }
+            const filteredData = this.options[0].data.filter(item => {
+                return item.toLowerCase().indexOf(text.toLowerCase()) > -1;
+            });
+            this.filteredOptions = [{ data: filteredData }];
+        }
     }
-  }
 };
 
 storiesOf("Vue-Autosuggest", module)
-  .add("simplest", () => ({
-    components: { Autosuggest },
-    template: `<div>
+    .add("simplest", () => ({
+        components: { Autosuggest },
+        template: `<div>
                     <div style="padding-top:10px; margin-bottom: 10px;"><span v-if="selected">You have selected {{selected}}</span></div>
                     <div>
                         <autosuggest :suggestions="filteredOptions" :inputProps="inputProps"/>
                     </div>
                 </div>`,
-    data() {
-      return {
-        selected: "",
-        filteredOptions: [],
-        options: [
-          {
-            data: sharedData.options.slice(0, 10)
-          }
-        ],
-        inputProps: {
-          id: "autosuggest__input",
-          onInputChange: this.onInputChange,
-          onClick: () => {
-            action("clicked-it");
-          },
-          initialValue: ""
-        }
-      };
-    },
-    methods: {
-      onInputChange(text) {
-        if (text === null) {
-          return;
-        }
-        const filteredData = this.options[0].data.filter(item => {
-          return item.toLowerCase().indexOf(text.toLowerCase()) > -1;
-        });
-        this.filteredOptions = [{data:filteredData}];
-      }
-    }
-  }))
-  .add("simple with section configs", () => ({
-    components: { Autosuggest },
-    template: `<div>
+        data() {
+            return {
+                selected: "",
+                filteredOptions: [],
+                options: [
+                    {
+                        data: sharedData.options.slice(0, 10)
+                    }
+                ],
+                inputProps: {
+                    id: "autosuggest__input",
+                    onInputChange: this.onInputChange,
+                    onClick: () => {
+                        action("clicked-it");
+                    },
+                    initialValue: ""
+                }
+            };
+        },
+        methods: sharedData.methods
+    }))
+    .add("simple with multiple sections", () => ({
+        components: { Autosuggest },
+        template: `<div>
                     <div style="padding-top:10px; margin-bottom: 10px;"><span v-if="selected">You have selected {{selected}}</span></div>
                     <div>
                         <autosuggest 
@@ -100,38 +82,61 @@ storiesOf("Vue-Autosuggest", module)
                                 />
                     </div>
                 </div>`,
-    data() {
-      return {
-        selected: "",
-        limit: 10,
-        filteredOptions: [],
-        options: [
-          {
-            data: sharedData.options
-          }
-        ],
-        sectionConfigs: {
-          default: {
-            limit: 6,
-            onSelected: (item, originalInput) => {
-              console.log(`Selected "${item.item}"`);
-            }
-          }
+        data() {
+            return {
+                selected: "",
+                limit: 10,
+                filteredOptions: [],
+                options: [
+                    {
+                        data: sharedData.options
+                    }
+                ],
+                sectionConfigs: {
+                    default: {
+                        limit: 6,
+                        onSelected: (item, originalInput) => {
+                            console.log(`Selected "${item.item}"`);
+                        }
+                    }
+                },
+                inputProps: {
+                    id: "autosuggest__input",
+                    initialValue: "",
+                    onClick: () => {},
+                    onInputChange: this.onInputChange,
+                    placeholder: "Type 'g'"
+                }
+            };
         },
-        inputProps: {
-          id: "autosuggest__input",
-          initialValue: "",
-          onClick: () => {},
-          onInputChange: this.onInputChange,
-          placeholder: "Type 'g'"
+        methods: {
+            onInputChange(text) {
+                if (text === null) {
+                    return;
+                }
+                const filtered = [];
+                const suggestionsData = this.options[0].data.filter(item => {
+                    return item.toLowerCase().indexOf(text.toLowerCase()) > -1;
+                });
+    
+                suggestionsData.length > 0 &&
+                    filtered.push(
+                        {
+                            label: "Section 1",
+                            data: suggestionsData
+                        },
+                        {
+                            label: "Section 2",
+                            data: suggestionsData
+                        }
+                    );
+                this.filteredOptions = filtered;
+            }
         }
-      };
-    },
-    methods: sharedData.methods
-  }))
-  .add("with property: initial value", () => ({
-    components: { Autosuggest },
-    template: `<div>
+    }))
+    .add("with property: initial value", () => ({
+        components: { Autosuggest },
+        template: `<div>
                     <div style="padding-top:10px; margin-bottom: 10px;"><span v-if="selected">You have selected {{selected}}</span></div>
                     <div>
                         <autosuggest 
@@ -141,32 +146,32 @@ storiesOf("Vue-Autosuggest", module)
                                 />
                     </div>
                 </div>`,
-    data() {
-      return {
-        selected: "",
-        limit: 10,
-        filteredOptions: [],
-        options: [
-          {
-            data: sharedData.options
-          }
-        ],
-        sectionConfigs: {
-          default: {
-            limit: 6,
-            onSelected: (item, originalInput) => {
-              console.log(`Selected "${item.item}"`);
-            }
-          }
+        data() {
+            return {
+                selected: "",
+                limit: 10,
+                filteredOptions: [],
+                options: [
+                    {
+                        data: sharedData.options
+                    }
+                ],
+                sectionConfigs: {
+                    default: {
+                        limit: 6,
+                        onSelected: (item, originalInput) => {
+                            console.log(`Selected "${item.item}"`);
+                        }
+                    }
+                },
+                inputProps: {
+                    id: "autosuggest__input",
+                    initialValue: "math",
+                    onClick: () => {},
+                    onInputChange: this.onInputChange,
+                    placeholder: "Type 'g'"
+                }
+            };
         },
-        inputProps: {
-          id: "autosuggest__input",
-          initialValue: "math",
-          onClick: () => {},
-          onInputChange: this.onInputChange,
-          placeholder: "Type 'g'"
-        }
-      };
-    },
-    methods: sharedData.methods
-  }));
+        methods: sharedData.methods
+    }));
