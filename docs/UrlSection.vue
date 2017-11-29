f5aa18914ba0c3ffd47966a7f88afb93a6f13977
darrenjennings <dmjenn02@gmail.com>
2017-11-28 22:26:17 -0800

    Exporting default section as component and extending it

--- docs/UrlSection.vue
+++ docs/UrlSection.vue
@@ -2,8 +2,8 @@
     <ul role="listbox" aria-labelledby="autosuggest">
         <li v-if="section.label" :class="className">{{section.label}}</li>
         <li v-for="(val, key) in list"
-            role="option" 
-            class="autosuggest__results_item" 
+            role="option"
+            class="autosuggest__results_item"
             :key="getItemIndex(key)" 
             :class="{'autosuggest__results_item-highlighted' : getItemIndex(key) == currentIndex, 'autosuggest__results_item':true}" 
             :data-suggestion-index="getItemIndex(key)"
@@ -11,52 +11,17 @@
             @mouseenter="onMouseEnter"
             @mouseleave="onMouseLeave"
             :id="`autosuggest__results_item-${getItemIndex(key)}`">{{val.value}}</li>
-        
     </ul>
 </template>
 
 <script>
+import DefaultSection from '../src/parts/DefaultSection.vue';
 export default {
   name: "url-section",
-  props: {
-    section: { type: Object, required: true },
-    currentIndex: { type: Number, required: false, default: Infinity },
-    updateCurrentIndex: { type: Function, required: true }
-  },
-  computed: {
-    list: function() {
-      var l = this.section.limit;
-      if (this.section.data.length < l) {
-        l = this.section.data.length;
-      }
-
-      return this.section.data.slice(0, l);
-    },
-    className: function() {
-      return (
-        "autosuggest__results_title " +
-        "autosuggest__results_title_" +
-        this.section.name
-      );
-    }
-  },
+  extends: DefaultSection,
   methods: {
-    getItemIndex(i) {
-      return this.section.start_index + i;
-    },
-    getItemByIndex(i) {
-      return this.section.data[i];
-    },
     getLabelByIndex(i) {
       return this.section.data[i].value;
-    },
-    onMouseEnter(event) {
-      this.updateCurrentIndex(
-        event.currentTarget.getAttribute("data-suggestion-index")
-      );
-    },
-    onMouseLeave() {
-      this.updateCurrentIndex(null);
     }
   }
 };
