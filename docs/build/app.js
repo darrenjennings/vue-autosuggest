de8402a1a816255b7e8c75d36eb17ed03d416162
Darren Jennings <dmjenn02@gmail.com>
2018-07-17 13:28:43 -0700

    chore(1.4.3) bump version and build

--- docs/build/app.js
+++ docs/build/app.js
@@ -655,7 +655,7 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
       section: { type: Object, required: true },
       currentIndex: { type: Number, required: false, default: Infinity },
       updateCurrentIndex: { type: Function, required: true },
-      searchInput: { type: String, required: false, default: "" },
+      searchInput: { type: [String, Number], required: false, default: "" },
       renderSuggestion: { type: Function, required: true },
       normalizeItemFunction: { type: Function, required: true }
     },
