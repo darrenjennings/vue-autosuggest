ba8d8a9fcb60a3bc017d42628028e2a7823a6ef6
Dan Wilson <work@danjwilson.co.uk>
2022-02-19 13:05:29 +0000

    chore(deps): Update rollup and plugins

--- build/rollup.umd.config.js
+++ build/rollup.umd.config.js
@@ -1,11 +1,11 @@
 import vue from "rollup-plugin-vue";
-import buble from "rollup-plugin-buble";
+import buble from "@rollup/plugin-buble";
 import filesize from "rollup-plugin-filesize";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import uglify from "rollup-plugin-uglify";
-import replace from "rollup-plugin-replace";
-import json from "rollup-plugin-json";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { uglify } from "rollup-plugin-uglify";
+import replace from "@rollup/plugin-replace";
+import json from "@rollup/plugin-json";
 
 export default {
   input: "src/vue-autosuggest.js",
